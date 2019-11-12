da84fb468cc90ff7d7974c61af4c743471a51a3b
SLoban <sloban@ibagroup.eu>
2019-11-12 11:48:41 +0300

    Initial Commit

---
+++ OTUS+RxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OTUS+RxSwift
+//
+//  Created by Sviatlana Loban on 11/12/19.
+//  Copyright © 2019 SLoban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
